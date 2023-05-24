386b0844b63c213fdeacb9605ff2897211bc83cf
Joanna Seybold <126947675+clever-girl31@users.noreply.github.com>
2023-05-23 22:51:51 -0500

    addEmployee() implemented

--- program.js
+++ program.js
@@ -127,6 +127,78 @@ function addRole() {
     })
 };
 
+const roleList = []
+function getRoles() {
+  connection.query('SELECT * FROM `role`', function (err, results) {
+    if (err) {
+      console.error(err);
+    } else {
+      for (let i = 0; i < results.length; i++) {
+        roleList.push(results[i].title)
+      }
+    }
+  })
+}
+getRoles()
+
+const empList = []
+function getEmps() {
+  connection.query('SELECT * FROM `employee`', function (err, results) {
+    if (err) {
+      console.error(err);
+    } else {
+      for (let i = 0; i < results.length; i++) {
+        empList.push(results[i].first_name + ' ' + results[i].last_name)
+      }
+    }
+  })
+}
+getEmps()
+
+function addEmployee() {
+  inquirer
+    .prompt([{
+      type: 'input',
+      name: 'newFirst',
+      message: 'Enter new employee first name:',
+    }, {
+      type: 'input',
+      name: 'newLast',
+      message: 'Enter new employee last name:'
+    }, {
+      type: 'list',
+      name: 'role',
+      message: 'Choose new employee role:',
+      choices: roleList,
+    }, {
+      type: 'list',
+      name: 'mgr',
+      message: 'Choose new employee manager:',
+      choices: empList
+    }])
+    .then((answers) => {
+      connection.query('SELECT COUNT(*) AS count FROM employee', function (err, results) {
+        var roleId = roleList.indexOf(answers.role) + 1
+        var mgrId = empList.indexOf(answers.mgr) + 1
+        var newEmpId = results[0].count + 1
+
+        connection.query(`INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (${newEmpId}, "${answers.newFirst}", "${answers.newLast}", ${roleId}, ${mgrId})`, function (err) {
+          if (err) {
+            console.error(err)
+          } else {
+            console.log(`New employee "${answers.newFirst} ${answers.newLast}" successfully added.`)
+            backOrQuit()
+          }
+        })
+      })
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.error(err)
+      }
+    })
+};
+
 function backOrQuit() {
     inquirer
       .prompt([
@@ -149,28 +221,28 @@ function backOrQuit() {
 
 function handleResponse(answers) {
   if (answers.q1 === 'View all departments') {
-    console.log('~~~Viewing Departments~~~\n')
+    console.log('***Viewing Departments***\n')
     viewDepts();
   };
   if (answers.q1 === 'View all roles') {
-    console.log('~~~Viewing Roles~~~\n')  
+    console.log('***Viewing Roles***\n')  
     viewRoles();
   } 
   if (answers.q1 === 'View all employees') {
-    console.log('~~~Viewing Employees~~~') 
+    console.log('***Viewing Employees***\n') 
     viewEmployees();
   }
   if (answers.q1 === 'Add a department') {
-    console.log('running addDepartment()')
+    console.log('***Add Department***\n')
     addDepartment();
   }
   if (answers.q1 === 'Add a role') {
-    console.log('running addRole()')
+    console.log('***Add Role***\n')
     addRole();
   }
   if (answers.q1 === 'Add an employee') {
     console.log('running addEmployee()')
-    //  addEmployee();
+     addEmployee();
   }
   if (answers.q1 === 'Update an employee role') {
     console.log('running updateEmpRole()')
